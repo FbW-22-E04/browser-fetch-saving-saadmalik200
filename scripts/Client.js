8ed260c780eca26771a4e985607bd62d96307cae
Saad Malik <saad_malik200@hotmail.com>
2022-10-05 16:32:20 +0200

    ex1 process

--- scripts/Client.js
+++ scripts/Client.js
@@ -1,14 +1,18 @@
 class Client {
-    constructor () {
-        //Your token goes here
-        this.token = "";
-    }
+  constructor() {
+    //Your token goes here
+    this.token = "9f010e4f";
+  }
 
-    async getMovieData(movie) {
-        let response = await fetch(`http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`);
-        let data = await response.json();
-        return data;
-    }
+  async getMovieData(movie) {
+    let response = await fetch(
+      `http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`
+      //   `http://www.omdbapi.com/?t=${movie}`
+    );
+    let data = await response.json();
+    // console.log(data);
+    return data;
+  }
 }
 
 console.log("Hello from Client.js");
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,49 @@
-import View from './View.js';
-import Client from './Client.js';
+import View from "./View.js";
+import Client from "./Client.js";
 
 // All of your javascript should go here
 console.log("Hello from index.js");
+
+const input = document.querySelector("input");
+const saveBtn = document.querySelector(".btn-save");
+const resetBtn = document.querySelector(".btn-reset");
+
+const newClient = new Client();
+const view = new View();
+
+const savedArray = [];
+
+loadTodos();
+
+input.addEventListener("change", async () => {
+  if (input.value) {
+    // console.log(input.value);
+
+    const data1 = await newClient.getMovieData(input.value);
+
+    savedArray.push(data1);
+    view.displayMovieOnPage(data1);
+    console.log(savedArray);
+    localStorage.setItem("movies", JSON.stringify(savedArray));
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  view.removeDisplay();
+});
+
+function loadTodos() {
+  const localStorageItems = localStorage.getItem("movies");
+  const parsed = JSON.parse(localStorageItems);
+
+  if (localStorageItems) {
+    savedArray.push(...parsed);
+    savedArray.forEach((item) => {
+      //   const li = document.createElement("li");
+      console.log(item);
+      //   li.textContent = item;
+      //   todoList.appendChild(li);
+      view.displayMovieOnPage(item);
+    });
+  }
+}
